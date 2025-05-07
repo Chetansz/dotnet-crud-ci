22312bd9befa2eec2d36d51c8325bfb0ea406657
Chetan Sagabala <Chetan.k@leadrat.com>
2025-05-07 10:59:51 +0530

    changesW1

---
+++ Tests/TodoControllerTests.cs
@@ -0,0 +1,15 @@
+using Xunit;
+using DotnetCrudApi.Controllers;
+using DotnetCrudApi.Models;
+
+namespace DotnetCrudApi.Tests;
+
+public class TodoControllerTests
+{
+    [Fact]
+    public void Test1()
+    {
+        // Placeholder test - will be implemented later
+        Assert.True(true);
+    }
+}
